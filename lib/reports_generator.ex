d5ea5b908d83fb5a723088f467b8c6743ba35bb1
Eduardo <eduardo.andrade15@hotmail.com>
2022-10-08 20:45:20 -0300

    created module parser

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,25 +1,14 @@
 defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
   def build(filename) do
-    "reports/#{filename}"
-    |> File.stream!()
-    |> Enum.reduce(report_acc(), &on_convert_line_in_map/2)
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), &sum_values/2)
   end
 
-  defp on_convert_line_in_map(line, report),
-    do:
-      line
-      |> parse_line()
-      |> map_put(report)
-
-  defp map_put([id, _food_name, price], report),
+  defp sum_values([id, _food_name, price], report),
     do: Map.put(report, id, report[id] + price)
-
-  defp parse_line(line),
-    do:
-      line
-      |> String.trim()
-      |> String.split(",")
-      |> List.update_at(2, &String.to_integer/1)
 
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
